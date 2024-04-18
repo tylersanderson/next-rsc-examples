ca3ad2a0936b2ab670a2c26dbf516678d1530605
Tyler Sanderson <Tyler Sanderson>
2024-04-18 12:20:41 -0500

    Renamed date page

--- src/app/date/page.tsx
+++ src/app/date/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./page.module.css";
 
-export default function Bundle() {
+export default function DatePage() {
   return (
     <main className={styles.main}>
       <h2>{new Date(Date.now()).toLocaleString()}</h2>
